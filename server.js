bd9ac495d3b3784921aaca91e1860514ae3f385e
Lior Hoshea <hoshealior@gmail.com>
2020-08-28 13:29:18 +0300

    changed config for heroku

--- server.js
+++ server.js
@@ -10,17 +10,24 @@ require('dotenv').config()
 const { PORT, REACT_APP_MONGODB_URI } = process.env
 mongoose.connect(REACT_APP_MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  })
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//   next()
+// })
 
+// "scripts": {
+//   "start": "react-scripts start",
+//   "build": "react-scripts build",
+//   "test": "react-scripts test",
+//   "eject": "react-scripts eject"
+// }
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', api)
 
 app.listen(PORT, () => {
